f3f240e262c28c22f80fa511b34d2265226e157e
Max Shapira <itsjustmax9@gmail.com>
2023-05-08 00:11:48 +0300

    fixed tip calculator

--- tip_calculations/tipCalc.js
+++ tip_calculations/tipCalc.js
@@ -1,16 +1,31 @@
 task19_tip_calculator = () => {
-    let totalInput = document.getElementById("total")
-    console.log(totalInput.value)
-    let rangeInput = document.getElementById("tip_percent")
-    let totalToPayDiv = document.createElement("to-pay")
-    rangeInput.addEventListener("change",() => {
-        let totalInputValue = Number(totalInput.value)
-        let tipPrecentage = Number(rangeInput.value)
-        let totalToPay = totalInputValue + totalInputValue * (tipPrecentage /100)
-        console.log(`Total Input: ${totalInputValue}\n Tip Precentage: ${tipPrecentage}\nTotal To Pay: ${totalToPay}`)
-        totalToPayDiv.innerHTML = totalToPay
+  let totalInput = document.getElementById("total");
+  let rangeInput = document.getElementById("tip_percent");
+  let totalToPayDiv = document.querySelector(".to-pay");
 
-    })
-}
+  rangeInput.addEventListener("change", () => {
+totalToPayDiv.innerHTML = ""
 
-task19_tip_calculator()
+    let totalInputValue = Number(totalInput.value);
+    let tipPrecentage = Number(rangeInput.value);
+    let totalToPay = totalInputValue + totalInputValue * (tipPrecentage / 100);
+    console.log(
+      `Total Input: ${totalInputValue}\n Tip Precentage: ${tipPrecentage}\nTotal To Pay: ${totalToPay}`
+    );
+
+    let current = document.createElement("p");
+    current.innerHTML = `Current Price: ${totalInputValue}`;
+
+    let tip = document.createElement("p");
+    tip.innerHTML = `Tip is ${tipPrecentage}% : ${
+      (totalInputValue * (tipPrecentage / 100)).toFixed(2)
+    }`;
+
+    let total = document.createElement("p");
+    total.innerHTML = `Total Price: ${totalToPay}`;
+
+    totalToPayDiv.appendChild(current);
+    totalToPayDiv.appendChild(tip);
+    totalToPayDiv.appendChild(total);
+  });
+};
